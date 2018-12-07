addeaf021add1e89267c9246fc852df385a92740
derwentx <derwentx@gmail.com>
2018-12-08 06:48:28 +1100

    comment unused demo code

--- src/demo.cpp
+++ src/demo.cpp
@@ -10,161 +10,161 @@
 #include "demo.h"
 #include "enc_spi.h"
 
-void demo_read_reg() {
-    byte result;
-    byte reg_addr;
-
-    static byte allregs[] = {
-        ERDPTL, ERDPTH, EWRPTL, EWRPTH, ETXSTL, ETXSTH, ETXNDL, ETXNDH, ERXSTL,
-        ERXSTH, ERXNDL, ERXNDH, ERXRDPTL, ERXRDPTH, ERXWRPTL, ERXWRPTH, EDMASTL,
-        EDMASTH, EDMANDL, EDMANDH, EDMADSTL, EDMADSTH, EDMACSL, EDMACSH,
-
-        EHT0, EHT1, EHT2, EHT3, EHT4, EHT5, EHT6, EHT7, EPMM0, EPMM1, EPMM2,
-        EPMM3, EPMM4, EPMM5, EPMM6, EPMM7, EPMCSL, EPMCSH, EPMOL, EPMOH, EWOLIE,
-        EWOLIR, ERXFCON, EPKTCNT,
-
-        MACON1, MACON3, MACON4, MABBIPG, MAIPGL, MAIPGH, MACLCON1, MACLCON2,
-        MAMXFLL, MAMXFLH, MAPHSUP, MICON, MICMD, MIREGADR, MIWRL, MIWRH, MIRDL,
-        MIRDH,
-
-        MAADR1, MAADR0, MAADR3, MAADR2, MAADR5, MAADR4, EBSTSD, EBSTCON,
-        EBSTCSL, EBSTCSH, MISTAT, EREVID, ECOCON, EFLOCON, EPAUSL, EPAUSH,
-    };
-
-    for (uint8_t i=0; i<ARRAY_LEN(allregs); i++) {
-        reg_addr = allregs[i];
-        result = enc_read_reg(reg_addr);
-        Serial.print(F("read register 0x"));
-        if(reg_addr < 0x10) {Serial.print(0);}
-        Serial.print(reg_addr, HEX);
-        Serial.print(F(", response: 0x"));
-        if(result < 0x10) {Serial.print(0);}
-        Serial.println(result, HEX);
-    }
-
-    // Should look like
-    // FA
-    // 05
-    // 00
-    // 00
-    // 00
-    // 00
-    // 00
-    // 00
-    // FA
-    // 05
-    // FF
-    // 1F
-    // FA
-    // 05
-    // 00 ...
-
-    // Works in SPI_MODE1, SPI_MODE3
-    // Doesn't work in SPI_MODE0
-}
-
-void demo_bitflip() {
-    byte result;
-
-    Serial.print(F("View the ECON1 register (should be initialized to 0x00): 0x"));
-    result = enc_read_reg(ECON1);
-    Serial.println(result, HEX);
-    Serial.print(F("Set the ECON1_RXEN bit field (Enable Recieve)"));
-    enc_bit_set(ECON1, ECON1_RXEN);
-    Serial.print(F("View the ECON1 register (should be set to 0x04): 0x"));
-    result = enc_read_reg(ECON1);
-    Serial.println(result, HEX);
-    Serial.print(F("clear the ECON1_RXEN bit field (Enable Recieve)"));
-    enc_bit_clr(ECON1, ECON1_RXEN);
-    Serial.print(F("View the ECON1 register (should be set to 0x00): 0x"));
-    result = enc_read_reg(ECON1);
-    Serial.println(result, HEX);
-}
-
-void demo_write_reg() {
-    byte result;
-    Serial.print(F("View the ERXSTL:ERXSTH registers (should be initialized to 0xfa, 0x05): 0x"));
-    result = enc_read_reg(ERXSTL);
-    Serial.print(result, HEX);
-    Serial.print(F(", 0x"));
-    // result = enc_read_reg(ERXSTH);
-    Serial.println(result, HEX);
-
-    Serial.println(F("set the ERXSTL:ERXSTH registers to (0xaa, 0x05):"));
-    enc_write_reg(ERXSTL, 0xaa);
-    // enc_write_reg(ERXSTH, 0x05);
-
-    Serial.print(F("View the ERXSTL:ERXSTH registers (should now be set to 0xaa, 0x05): 0x"));
-    result = enc_read_reg(ERXSTL);
-    Serial.print(result, HEX);
-    Serial.print(F(", 0x"));
-    // result = enc_read_reg(ERXSTH);
-    Serial.println(result, HEX);
-
-    Serial.println(F("return the ERXSTL:ERXSTH registers to (0xfa, 0x05):"));
-    enc_write_reg(ERXSTL, 0xfa);
-    // enc_write_reg(ERXSTH, 0x05);
-
-    // Doesn't work in SPI_MODE1, SPI_MODE2,
-    // Works in SPI_MODE3
-}
-
-void demo_write_reg_2() {
-    byte result;
-    Serial.print(F("View the ERXNDL:ERXNDH registers (should be initialized to 0xff, 0x1f): 0x"));
-    result = enc_read_reg(ERXNDL);
-    Serial.print(result, HEX);
-    Serial.print(F(", 0x"));
-    result = enc_read_reg(ERXNDH);
-    Serial.println(result, HEX);
-
-    Serial.println(F("set the ERXNDL:ERXNDH registers to (0xff, 0x07):"));
-    enc_write_reg(ERXNDL, 0xff);
-    enc_write_reg(ERXNDH, 0x07);
-
-    Serial.print(F("View the ERXNDL:ERXNDH registers (should now be set to 0xff, 0x07): 0x"));
-    result = enc_read_reg(ERXNDL);
-    Serial.print(result, HEX);
-    Serial.print(F(", 0x"));
-    result = enc_read_reg(ERXNDH);
-    Serial.println(result, HEX);
-
-    Serial.println(F("return the ERXNDL:ERXNDH registers to (0xff, 0x1f):"));
-    enc_write_reg(ERXNDL, 0xff);
-    enc_write_reg(ERXNDH, 0x1f);
-}
-
-void demo_buf_write() {
-    static byte testdata[] = {
-        0x00, 0x00, 0x00, 0x00, // START Frame
-        0xf0, 0xff, 0x00, 0x00, // Blue
-        0xf0, 0x00, 0xff, 0x00, // Green
-        0xf0, 0x00, 0x00, 0xff, // Red
-        0xff, 0xff, 0xff, 0xff, // END Frame
-    };
-    byte * results = (byte *) malloc(sizeof(testdata));
-    byte result;
-
-    Serial.println(F("Set buf write pointer (EWRPTL:EWRPTH) byte (0x02:0x03) to 0x0000"));
-    enc_write_reg(EWRPTL, 0x00);
-    enc_write_reg(EWRPTH, 0x00);
-    Serial.println(F("Write bytes to buffer"));
-
-    enc_write_buf(testdata, ARRAY_LEN(testdata));
-    Serial.println(F("Set buf read pointer (ERDPTL:ERDPTH) byte (0x00:0x01) to 0x0000"));
-    enc_write_reg(ERDPTL, 0x00);
-    enc_write_reg(ERDPTH, 0x00);
-    Serial.print(F("read bytes from buffer: 0x"));
-
-    enc_read_buf(results, ARRAY_LEN(testdata));
-
-    for(uint8_t i=0; i<ARRAY_LEN(testdata); i++){
-        result = results[i];
-        if(result < 0x10) {Serial.print(0);}
-        Serial.print(result, HEX);
-    }
-    Serial.println();
-}
+// void demo_read_reg() {
+//     byte result;
+//     byte reg_addr;
+//
+//     static byte allregs[] = {
+//         ERDPTL, ERDPTH, EWRPTL, EWRPTH, ETXSTL, ETXSTH, ETXNDL, ETXNDH, ERXSTL,
+//         ERXSTH, ERXNDL, ERXNDH, ERXRDPTL, ERXRDPTH, ERXWRPTL, ERXWRPTH, EDMASTL,
+//         EDMASTH, EDMANDL, EDMANDH, EDMADSTL, EDMADSTH, EDMACSL, EDMACSH,
+//
+//         EHT0, EHT1, EHT2, EHT3, EHT4, EHT5, EHT6, EHT7, EPMM0, EPMM1, EPMM2,
+//         EPMM3, EPMM4, EPMM5, EPMM6, EPMM7, EPMCSL, EPMCSH, EPMOL, EPMOH, EWOLIE,
+//         EWOLIR, ERXFCON, EPKTCNT,
+//
+//         MACON1, MACON3, MACON4, MABBIPG, MAIPGL, MAIPGH, MACLCON1, MACLCON2,
+//         MAMXFLL, MAMXFLH, MAPHSUP, MICON, MICMD, MIREGADR, MIWRL, MIWRH, MIRDL,
+//         MIRDH,
+//
+//         MAADR1, MAADR0, MAADR3, MAADR2, MAADR5, MAADR4, EBSTSD, EBSTCON,
+//         EBSTCSL, EBSTCSH, MISTAT, EREVID, ECOCON, EFLOCON, EPAUSL, EPAUSH,
+//     };
+//
+//     for (uint8_t i=0; i<ARRAY_LEN(allregs); i++) {
+//         reg_addr = allregs[i];
+//         result = enc_read_reg(reg_addr);
+//         Serial.print(F("read register 0x"));
+//         if(reg_addr < 0x10) {Serial.print(0);}
+//         Serial.print(reg_addr, HEX);
+//         Serial.print(F(", response: 0x"));
+//         if(result < 0x10) {Serial.print(0);}
+//         Serial.println(result, HEX);
+//     }
+//
+//     // Should look like
+//     // FA
+//     // 05
+//     // 00
+//     // 00
+//     // 00
+//     // 00
+//     // 00
+//     // 00
+//     // FA
+//     // 05
+//     // FF
+//     // 1F
+//     // FA
+//     // 05
+//     // 00 ...
+//
+//     // Works in SPI_MODE1, SPI_MODE3
+//     // Doesn't work in SPI_MODE0
+// }
+//
+// void demo_bitflip() {
+//     byte result;
+//
+//     Serial.print(F("View the ECON1 register (should be initialized to 0x00): 0x"));
+//     result = enc_read_reg(ECON1);
+//     Serial.println(result, HEX);
+//     Serial.print(F("Set the ECON1_RXEN bit field (Enable Recieve)"));
+//     enc_bit_set(ECON1, ECON1_RXEN);
+//     Serial.print(F("View the ECON1 register (should be set to 0x04): 0x"));
+//     result = enc_read_reg(ECON1);
+//     Serial.println(result, HEX);
+//     Serial.print(F("clear the ECON1_RXEN bit field (Enable Recieve)"));
+//     enc_bit_clr(ECON1, ECON1_RXEN);
+//     Serial.print(F("View the ECON1 register (should be set to 0x00): 0x"));
+//     result = enc_read_reg(ECON1);
+//     Serial.println(result, HEX);
+// }
+//
+// void demo_write_reg() {
+//     byte result;
+//     Serial.print(F("View the ERXSTL:ERXSTH registers (should be initialized to 0xfa, 0x05): 0x"));
+//     result = enc_read_reg(ERXSTL);
+//     Serial.print(result, HEX);
+//     Serial.print(F(", 0x"));
+//     // result = enc_read_reg(ERXSTH);
+//     Serial.println(result, HEX);
+//
+//     Serial.println(F("set the ERXSTL:ERXSTH registers to (0xaa, 0x05):"));
+//     enc_write_reg(ERXSTL, 0xaa);
+//     // enc_write_reg(ERXSTH, 0x05);
+//
+//     Serial.print(F("View the ERXSTL:ERXSTH registers (should now be set to 0xaa, 0x05): 0x"));
+//     result = enc_read_reg(ERXSTL);
+//     Serial.print(result, HEX);
+//     Serial.print(F(", 0x"));
+//     // result = enc_read_reg(ERXSTH);
+//     Serial.println(result, HEX);
+//
+//     Serial.println(F("return the ERXSTL:ERXSTH registers to (0xfa, 0x05):"));
+//     enc_write_reg(ERXSTL, 0xfa);
+//     // enc_write_reg(ERXSTH, 0x05);
+//
+//     // Doesn't work in SPI_MODE1, SPI_MODE2,
+//     // Works in SPI_MODE3
+// }
+//
+// void demo_write_reg_2() {
+//     byte result;
+//     Serial.print(F("View the ERXNDL:ERXNDH registers (should be initialized to 0xff, 0x1f): 0x"));
+//     result = enc_read_reg(ERXNDL);
+//     Serial.print(result, HEX);
+//     Serial.print(F(", 0x"));
+//     result = enc_read_reg(ERXNDH);
+//     Serial.println(result, HEX);
+//
+//     Serial.println(F("set the ERXNDL:ERXNDH registers to (0xff, 0x07):"));
+//     enc_write_reg(ERXNDL, 0xff);
+//     enc_write_reg(ERXNDH, 0x07);
+//
+//     Serial.print(F("View the ERXNDL:ERXNDH registers (should now be set to 0xff, 0x07): 0x"));
+//     result = enc_read_reg(ERXNDL);
+//     Serial.print(result, HEX);
+//     Serial.print(F(", 0x"));
+//     result = enc_read_reg(ERXNDH);
+//     Serial.println(result, HEX);
+//
+//     Serial.println(F("return the ERXNDL:ERXNDH registers to (0xff, 0x1f):"));
+//     enc_write_reg(ERXNDL, 0xff);
+//     enc_write_reg(ERXNDH, 0x1f);
+// }
+//
+// void demo_buf_write() {
+//     static byte testdata[] = {
+//         0x00, 0x00, 0x00, 0x00, // START Frame
+//         0xf0, 0xff, 0x00, 0x00, // Blue
+//         0xf0, 0x00, 0xff, 0x00, // Green
+//         0xf0, 0x00, 0x00, 0xff, // Red
+//         0xff, 0xff, 0xff, 0xff, // END Frame
+//     };
+//     byte * results = (byte *) malloc(sizeof(testdata));
+//     byte result;
+//
+//     Serial.println(F("Set buf write pointer (EWRPTL:EWRPTH) byte (0x02:0x03) to 0x0000"));
+//     enc_write_reg(EWRPTL, 0x00);
+//     enc_write_reg(EWRPTH, 0x00);
+//     Serial.println(F("Write bytes to buffer"));
+//
+//     enc_write_buf(testdata, ARRAY_LEN(testdata));
+//     Serial.println(F("Set buf read pointer (ERDPTL:ERDPTH) byte (0x00:0x01) to 0x0000"));
+//     enc_write_reg(ERDPTL, 0x00);
+//     enc_write_reg(ERDPTH, 0x00);
+//     Serial.print(F("read bytes from buffer: 0x"));
+//
+//     enc_read_buf(results, ARRAY_LEN(testdata));
+//
+//     for(uint8_t i=0; i<ARRAY_LEN(testdata); i++){
+//         result = results[i];
+//         if(result < 0x10) {Serial.print(0);}
+//         Serial.print(result, HEX);
+//     }
+//     Serial.println();
+// }
 
 int led_buffer_size = 4 * (N_LEDS + 2);
 byte * led_data = (byte *) malloc(led_buffer_size * sizeof(byte));
@@ -318,38 +318,38 @@ void demo_rainbows() {
 
  */
 
-void demo_mac_interference() {
-    byte reg;
-
-    /* enable MAC receive, TX/RX Pause = 0x0d */
-    reg = 0x00;
-    reg |= MACON1_MARXEN; // Enable MAC to recieve frames
-    reg |= MACON1_TXPAUS; // Allow IEEE defined flow control to function
-    reg |= MACON1_RXPAUS; // Allow IEEE defined flow control to function
-    enc_write_reg(MACON1, reg);
-
-    enc_reg_print("MACON1 should be 0x0d", MACON1);
-    enc_reg_print("MACON3", MACON3);
-
-    reg = 0x00;
-    // All short frames will be zero padded to 60 bytes and a valid CRC
-    // will then be appended
-    // reg |= MACON3_PADCFG0;
-    // MAC will append a valid CRC to all frames transmitted regardless of
-    // PADCFG bits. TXCRCEN must be set if the PADCFG bits specify that a
-    // valid CRC will be appended
-    // reg |= MACON3_TXCRCEN;
-    // The type/length field of transmitted and received frames will be
-    // checked. If it represents a length, the frame size will be compared
-    // and mismatches will be reported in the transmit/receive status vector.
-    // reg |= MACON3_FRMLNEN;
-    //
-    reg |= MACON3_FULDPX;
-    enc_write_reg(MACON3, reg);
-
-    enc_reg_print("MACON1 should be 0x0d", MACON1);
-    enc_reg_print("MACON3", MACON3);
-}
+// void demo_mac_interference() {
+//     byte reg;
+//
+//     /* enable MAC receive, TX/RX Pause = 0x0d */
+//     reg = 0x00;
+//     reg |= MACON1_MARXEN; // Enable MAC to recieve frames
+//     reg |= MACON1_TXPAUS; // Allow IEEE defined flow control to function
+//     reg |= MACON1_RXPAUS; // Allow IEEE defined flow control to function
+//     enc_write_reg(MACON1, reg);
+//
+//     Serial.print(F("MACON1 should be 0x0d: ")); enc_reg_print(MACON1); Serial.println();
+//     Serial.print(F("MACON3: ")); enc_reg_print(MACON3); Serial.println();
+//
+//     reg = 0x00;
+//     // All short frames will be zero padded to 60 bytes and a valid CRC
+//     // will then be appended
+//     // reg |= MACON3_PADCFG0;
+//     // MAC will append a valid CRC to all frames transmitted regardless of
+//     // PADCFG bits. TXCRCEN must be set if the PADCFG bits specify that a
+//     // valid CRC will be appended
+//     // reg |= MACON3_TXCRCEN;
+//     // The type/length field of transmitted and received frames will be
+//     // checked. If it represents a length, the frame size will be compared
+//     // and mismatches will be reported in the transmit/receive status vector.
+//     // reg |= MACON3_FRMLNEN;
+//     //
+//     reg |= MACON3_FULDPX;
+//     enc_write_reg(MACON3, reg);
+//
+//     Serial.print(F("MACON1 should be 0x0d: ")); enc_reg_print(MACON1); Serial.println();
+//     Serial.print(F("MACON3: ")); enc_reg_print(MACON3); Serial.println();
+// }
 
 /**
  * Receive ethernet packets from the ENC
